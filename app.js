349bca3d7048af2d956d41a8294b8d8d47d68424
DevinciCodes <devinlatham20@gmail.com>
2023-06-28 15:46:00 -0500

    added pg database

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const passport = require('passport');
 passportLocalMongoose = require('passport-local-mongoose')
 const methodOverride = require('method-override');
 const { stringify } = require("querystring");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' }).single('file');
 const app = express();
 
 
@@ -103,8 +105,17 @@ app.get('/letters', function(req,res){
 });
 
 app.get('/band-director-letters', function(req,res){
-  res.render('banddirectorletters')
+  const query = 'SELECT * FROM letters';
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.error('Error retrieving files from the database:', err);
+      return;
+    }
+    const files = result.rows;
+    res.render('banddirectorletters', { letters }); // Render a template or send the files data to the client
+  });
 });
+  
 
 app.get('/choir-director-letters', function(req,res){
   res.render('choirdirectorletters')
@@ -118,7 +129,72 @@ app.get('/musical-theater-director-letters', function(req,res){
   res.render('musicaltheaterdirectorletters')
 });
 
+app.get('/add-letters', function(req,res){
+  res.render('addletters')
+});
+
+
+
+//add letter
+app.post('/upload', upload,  (req, res) => {
+  const file = req.file;
+  console.log(req.body.file)
+
+  // Save the file details to the database
+  const query = 'INSERT INTO letter (filename, filedata) VALUES ($1, $2)';
+  const values = [file.originalname, file.buffer];
+  
+  pool.query(query, values, (err, result) => {
+    if (err) {
+      console.error('Error inserting file into the database:', err);
+      return;
+    }
+    console.log('File uploaded and saved to the database');
+    res.redirect('/banddirectorletters'); // Redirect to the page that displays uploaded files
+  });
+});
+
+
+//dl letter
+app.get('/download/:id', (req, res) => {
+  const fileId = req.params.id;
+  const query = 'SELECT * FROM your_table_name WHERE id = $1';
+  const values = [fileId];
+  pool.query(query, values, (err, result) => {
+    if (err) {
+      console.error('Error retrieving file from the database:', err);
+      return;
+    }
+    const file = result.rows[0];
+    res.set('Content-Disposition', `attachment; filename="${file.filename}"`);
+    res.send(file.filedata);
+  });
+});
+
+
+
+
+
+///sql connection
+
+
+const db = require('./db');
+
+db.query('SELECT * FROM directors_letters_db.lettercategories', (err, results) => {
+  if (err) {
+    console.error('Error executing query:', err);
+    return;
+  }
+  console.log('Query results:', results.rows);
+});
+
+
+
 const PORT = process.env.PORT || 3100
+
+
+
+
 
 app.listen(PORT, function() {
   console.log(`Server started on port ${PORT}`);
